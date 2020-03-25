7d6e590f38941ca1ad5351f24e137c7b0a6ebd35
jahorinac <registracijae@gmail.com>
2020-03-25 14:03:27 +0100

    T01 - task 01

---
+++ 01.js
@@ -0,0 +1,25 @@
+//01
+let student = {
+    name:"Car",
+    sclass:"IV",
+    regular: true
+};
+
+console.log(Object.keys(student)) // list (keys) of properties
+console.log(Object.values(student)) //list (values) of properties
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//console.log(_keys({name:, sclass:, shool:, }))
---
+++ z_text_of_tasks.md
@@ -0,0 +1,9 @@
+    
+1.    Write a JavaScript program to list the properties of a JavaScript object.
+    Sample object:
+        var student = {
+        name:"Car",
+        sclass:"IV",
+        regular: true
+    };
+    Sample Output: name,sclass,rollno
