4190504707305b6774ee59df575cc5eaf500761b
jahorinac <registracijae@gmail.com>
2020-03-25 15:18:41 +0100

    01 - added basic tasks

--- 01.js
+++ 01.js
@@ -1,13 +1,32 @@
 //01
+
 let student = {
     name:"Car",
-    sclass:"IV",
+    sclass: 5,
     regular: true
 };
 
-console.log(Object.keys(student)) // list (keys) of properties
-console.log(Object.values(student)) //list (values) of properties
 
+//console.log(Object.keys(student)) // list (keys) of properties
+//console.log(Object.values(student)) //list (values) of properties
+
+//02
+
+// let studentArray = Object.values(student);
+// let studentObject = Object.assign(studentArray);
+
+// console.log(studentObject);
+
+//03
+
+// delete student.regular;
+// console.log(student);
+
+//04
+
+//console.log(Object.keys(student).length);
+
+//05
 
 
 
--- z_text_of_tasks.md
+++ z_text_of_tasks.md
@@ -1,9 +1,15 @@
     
-1.    Write a JavaScript program to list the properties of a JavaScript object.
+01.    Write a JavaScript program to list the properties of a JavaScript object.
     Sample object:
         var student = {
         name:"Car",
-        sclass:"IV",
+        sclass: 5,
         regular: true
     };
     Sample Output: name,sclass,rollno
+    
+02. Covert array (from task 01) to object 
+
+03. Write a JavaScript program to delete the regular property from the following object. Also print the object before or after deleting the property.
+
+04. Length of object?
